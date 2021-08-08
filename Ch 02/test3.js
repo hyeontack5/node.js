08e435403681c1ac58be2077145c1a73c6e62ac4
hyeontack5 <sksgusxor@gmail.com>
2021-08-08 19:58:55 +0900

    주석 변경

--- Ch 02/test3.js
+++ Ch 02/test3.js
@@ -3,7 +3,7 @@ console.dir(process.argv);
 
 // 아래와 같은 결과가 출력됩니다. 자바스크립트 파일을 node 실행파일을 사용했을 때,
 // 두 개의 파라미터가 사용된다는 것을 알 수 있습니다.
-// process도 전형적인 전역객체 입니다.
+// process도 대표적인 전역객체 입니다.
 // argv 속성의 파라미터 수 : 2
 // [
 //   '/usr/local/Cellar/node/16.5.0/bin/node',
